ac5cb295ff4b1b427874d43ec062ed827aae6bff
snehasis007 <snehasis007@gmail.com>
2024-06-16 22:03:42 -0400

    Initial Commit

---
+++ App1/main.c
@@ -0,0 +1,14 @@
+//
+//  main.c
+//  App1
+//
+//  Created by snehasis chandra on 6/16/24.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
